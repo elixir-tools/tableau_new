0edf0b28d075a738cb67055de4405dbe25684dcf
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-03-11 17:40:36 -0400

    chore: remove dead code

--- lib/tableau_new.ex
+++ lib/tableau_new.ex
@@ -1,18 +1,2 @@
 defmodule TableauNew do
-  @moduledoc """
-  Documentation for `TableauNew`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TableauNew.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
